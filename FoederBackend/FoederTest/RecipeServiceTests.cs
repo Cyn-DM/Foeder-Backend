c5dbcf7e73cdcff411fc079ae5218e7342e049aa
Cynthia De Meester <439122@student.fontys.nl>
2024-10-10 10:36:46 +0200

    Added test for generating recipe response

---
+++ FoederBackend/FoederTest/RecipeServiceTests.cs
@@ -0,0 +1,38 @@
+using FoederBusiness.Dtos;
+using FoederBusiness.Interfaces;
+using FoederDomain.Interfaces;
+using FoederTest.Mocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FoederTest
+{
+    public class RecipeServiceTests
+    {
+        private readonly IRecipeService _recipeService;
+        public RecipeServiceTests()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<IRecipeService, RecipeServiceMock>();
+            services.AddTransient<IRecipeRepository, RecipeRepositoryMock>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            _recipeService = serviceProvider.GetService<IRecipeService>()!;
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AssertGetRecipeResponses()
+        {
+            List<GetRecipesResponse> responses = _recipeService.GetRecipes();
+
+            Assert.IsNotEmpty(responses);
+        }
+    }
+}
