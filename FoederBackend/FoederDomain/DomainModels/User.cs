1075d4b59788c17e94fef636012423f55c9d4623
Cynthia De Meester <439122@student.fontys.nl>
2024-11-08 14:06:16 +0100

    fixed last name bug

--- FoederBackend/FoederDomain/DomainModels/User.cs
+++ FoederBackend/FoederDomain/DomainModels/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
 
 namespace FoederDomain.DomainModels;
 
@@ -9,8 +10,8 @@ public class User
     public Guid Id { get; set; }
     [Required, MinLength(1), MaxLength(50)]
     public string FirstName { get; set; } = string.Empty;
-    [Required, MinLength(1), MaxLength(60)]
-    public string LastName { get; set; } = string.Empty;
+    [MinLength(1), MaxLength(60)]
+    public string? LastName { get; set; } = string.Empty;
     [NotMapped]
     public string FullName => FirstName + " " + LastName;
     [EmailAddress, MinLength(3), MaxLength(320)]
